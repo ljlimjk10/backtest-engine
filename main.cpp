ef5f1218175597b2fd31026182a6ee39dcd297f5
ljlimjk <ljlimjk10@gmail.com>
2025-06-22 19:08:55 +0800

    init BarFeed

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
+#include "include/engine/data/BarFeed.h"
+#include "include/engine/data/CsvBarSource.h"
+#include "include/engine/data/DataLoader.h"
+#include "libs/OrderBook/include/threadPool.h"
+#include "libs/OrderBook/include/orderBookManager.h"
+
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	ThreadPool threadPool;
+	OrderBookManager orderBookMgr(threadPool);
+
+	DataLoader barLoader("bar.csv");
+	CsvBarSource csvBarSource(barLoader.begin(), barLoader.end());
+	BarFeed<CsvBarSource> barFeed(std::move(csvBarSource));
 	return 0;
 }
